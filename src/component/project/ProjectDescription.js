2a7c7171baafa106f941b679b2f1be32a6f0c332
harshit107 <harshit107.in@gmail.com>
2023-05-09 00:56:29 +0530

    Complete project w/o aniumation

--- src/component/project/ProjectDescription.js
+++ src/component/project/ProjectDescription.js
@@ -6,6 +6,33 @@ const ProjectDescription = [
   {
     name: "Digital-Classroom",
     description:
+      "A fullstack app that allows for seamless classroom management,customized attendance system, live video meetings with 100% automatic mailing system.",
+    codeLink: "https://github.com/Harshit107/DigitalClassroom",
+    liveLink: "https://Harshit107.tech",
+    image: digitalClassroomImg,
+    tech: ["Android", "NodeJs", "ExpressJs", "MongoDB", "Firebase", "AWS"],
+  },
+  {
+    name: "App-Store",
+    description:
+      "An Android App similar to Google Play Store with features such as developer dynamic control, draft saving options, JWT authentication system, and more.",
+    codeLink: "https://github.com/Harshit107/appstore",
+    liveLink: "https://appstorein.netlify.app",
+    tech: ["Android", "NodeJs", "ExpressJs", "MongoDB", "Firebase", "AWS"],
+    image: appStoreImg,
+  },
+  {
+    name: "Expensify",
+    description:
+      "A React application for personal expense management features graph representations using eChar.js, smart filters, sorting, searching, and Google based authentication system.  ",
+    codeLink: "https://github.com/Harshit107/expensify",
+    liveLink: "https://myexpensify.netlify.app",
+    image: expensifyImg,
+    tech: ["React", "Javascript", "Firebase"],
+  },
+  {
+    name: "Digital-Classroom",
+    description:
       "A fullstack app that allows for seamless classroom management,customized attendance system, live video meetings",
     codeLink: "https://github.com/Harshit107/DigitalClassroom",
     liveLink: "https://Harshit107.tech",
