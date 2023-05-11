ea549e7811ae4a3d1465f08e39fa7bec679dd7a6
harshit107 <harshit107.in@gmail.com>
2023-05-12 01:45:35 +0530

    project update

--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -1,18 +1,15 @@
 import './Project.css' 
 import Header from './../Header/Header';
 import ProjectItemContainer from './ProjectItemContainer';
-import { motion, useScroll } from "framer-motion/dist/framer-motion";
 
 
 
 const Project = (props) => { 
-   const { scrollYProgress } = useScroll();
    const projectMainClassName = `project__main ${props.className}` 
    const initialDelay = 0.5;
    return (
-     <motion.div
+     <div
        className={projectMainClassName}
-       style={{ scaleX: scrollYProgress }}
      >
        <div className={"project__container"}>
          <Header heading="My Projects" />
@@ -20,7 +17,7 @@ const Project = (props) => {
            <ProjectItemContainer initialDelay={initialDelay} />
          </div>
        </div>
-     </motion.div>
+     </div>
    );
 }
 export default Project;
